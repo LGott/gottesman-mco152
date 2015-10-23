dfed1d9106359a31e357a741cc5bdc6f0a996aea
Leba Gottesman <leebzgott@gmail.com>
2015-10-22 20:51:07 -0700

    Updated Anagrams

--- src/gottesman/anagrams/MostAnagrams.java
+++ src/gottesman/anagrams/MostAnagrams.java
@@ -22,46 +22,52 @@ public class MostAnagrams {
 		// Read the file into the dicWords arrayList
 
 		while (readFile.hasNext()) {
-           dicWords.add(readFile.next());
+			dicWords.add(readFile.next());
 		}
 		return dicWords;
 	}
 
 	public ArrayList<ArrayList<String>> getAnagrams(String fileName) throws IOException {
 
-		//Create a new HashMap where the value of each key (key =specific letters), is an 
-		//arrayList of anagrams for those letters
-		
+		// Create a new HashMap where the value of each key (key =specific
+		// letters), is an
+		// arrayList of anagrams for those letters
+
 		Map<String, ArrayList<String>> listOfAnagrams = new HashMap<String, ArrayList<String>>();
-		
-		//return the arrayList of dictionary words into an arrayList
-		
+
+		// return the arrayList of dictionary words into an arrayList
+
 		ArrayList<String> listOfWords = fileRead(fileName);
 
-		//Place each word into a char array in order to sort the letters alphabetically. 
-		//(So that the words can be compared in order to find anagrams)
-		
+		// Place each word into a char array in order to sort the letters
+		// alphabetically.
+		// (So that the words can be compared in order to find anagrams)
+
 		for (String s : listOfWords) {
 			char[] c = s.toUpperCase().toCharArray();
 			Arrays.sort(c);
-			
-			//Retrieve the value of the key from the HashMap. If null, then create a new ArrayList for the map.
-			//Add the word into the new list or the existing list
-			
+
+			// Retrieve the value of the key from the HashMap. If null, then
+			// create a new ArrayList for the map.
+			// Add the word into the new list or the existing list
+
 			ArrayList<String> keyValues = listOfAnagrams.get(String.valueOf(c));
-		    if (keyValues == null) {
+			if (keyValues == null) {
 				keyValues = new ArrayList<String>();
 			}
-			
+
 			keyValues.add(s);
-			
-			//Put the keys and values into the HashMap
+
+			// Put the keys and values into the HashMap
 			listOfAnagrams.put(String.valueOf(c), keyValues);
+
 		}
-		
-		//If a value contains only one word in it's list, then it obviously does not have any anagrams, so we do not 
-		//want to return those words as part of the list, and therefore return only values that are greater than 1
-		
+
+		// If a value contains only one word in it's list, then it obviously
+		// does not have any anagrams, so we do not
+		// want to return those words as part of the list, and therefore return
+		// only values that are greater than 1
+
 		ArrayList<ArrayList<String>> anagrams = new ArrayList<ArrayList<String>>();
 		for (Map.Entry<String, ArrayList<String>> entry : listOfAnagrams.entrySet()) {
 			if (entry.getValue().size() > 1) {
